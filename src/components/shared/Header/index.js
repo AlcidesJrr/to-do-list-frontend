faa5a0b1158a748ee8d6466acb22edb15a363c26
Alcides dos Santos Junior <alcides.jrr@gmail.com>
2021-11-14 19:11:18 -0300

    commit

---
+++ src/components/shared/Header/index.js
@@ -0,0 +1,36 @@
+import './Styles.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <nav className="navbar  navbar-expand-lg navbar-light bg-black w-100">
+      <div className="container">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <Link className="navbar-brand text-white " to="/">
+          To Do List
+        </Link>
+        <div className="nav-right">
+        <div className="collapse navbar-collapse " id="navbarTogglerDemo03">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link text-white active" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/cadastro">
+                Cadastro
+              </Link>
+            </li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FcAlarmClock } from "react-icons/fc";
+
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center h1">Baita relógio < FcAlarmClock /></h1>
+    </div>
+  )
+}
+
+export default Home
