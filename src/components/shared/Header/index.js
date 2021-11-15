ff52ed33b383ad737bf3e9d7e81a1a783c03d4ab
Alcides dos Santos Junior <alcides.jrr@gmail.com>
2021-11-14 22:03:30 -0300

    commit

--- src/components/shared/Header/index.js
+++ src/components/shared/Header/index.js
@@ -1,10 +1,12 @@
 import './Styles.css'
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FcHome } from "react-icons/fc";
+import { FcTodoList } from "react-icons/fc";
 
 const Header = () => {
   return (
-    <nav className="navbar  navbar-expand-lg navbar-light bg-black w-100">
+    <nav className="navbar  navbar-expand-lg navbar-light w-100">
       <div className="container">
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -16,13 +18,13 @@ const Header = () => {
         <div className="collapse navbar-collapse " id="navbarTogglerDemo03">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link text-white active" to="/">
-                Home
+              <Link className="nav-link text-white active" title="Home" to="/">
+                < FcHome size={20} /> 
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-white" to="/cadastro">
-                Cadastro
+              <Link className="nav-link text-white" title= "Cadastrar Tarefa" to="/cadastro">
+              < FcTodoList size={20} /> 
               </Link>
             </li>
           </ul>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import './Styles.css';
+import React from 'react';
 import { FcAlarmClock } from "react-icons/fc";
 
 
