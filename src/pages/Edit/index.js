87d134e0f07cda1c499b3984b4f05ebe529b8c38
Alcides dos Santos Junior <alcides.jrr@gmail.com>
2021-11-18 13:33:31 -0300

    add modal

--- src/pages/Edit/index.js
+++ src/pages/Edit/index.js
@@ -1,82 +1,131 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Api from '../../api/api';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-import { IoIosReturnLeft} from "react-icons/io";
+import { IoIosReturnLeft } from 'react-icons/io';
 
 const Edit = () => {
     const [list, setList] = useState({});
-   
+
     useEffect(() => {
         getListById();
-      }, [])
+    }, []);
 
-    const { id }= useParams();
+    const { id } = useParams();
     const getListById = async () => {
         const request = await Api.fetchGetById(id);
         const list = await request.json();
         setList(list);
-        }
+    };
 
-        const handleFieldsChange = (evento) => {
-            const campos = { ...list }
-            campos[evento.target.name] = evento.target.value;
-            campos[evento.target.descricao] = evento.target.value;
-            campos[evento.target.status] = evento.target.value;
-            campos[evento.target.prazo] = evento.target.value;
+    const handleFieldsChange = evento => {
+        const campos = { ...list };
+        campos[evento.target.name] = evento.target.value;
+        campos[evento.target.descricao] = evento.target.value;
+        campos[evento.target.status] = evento.target.value;
+        campos[evento.target.prazo] = evento.target.value;
 
-            setList(campos);
-         }
-   
+        setList(campos);
+    };
+
     return (
         <>
-        <div className="titulo-h1">
-        <h1>Editar Tarefa</h1>
-        </div>
-        <div className="cadastro">
-            <form>
-                <div className="form-group">
-                    <label htmlFor="titulo">Título</label>
-                    <input type="text"  value={list.titulo} onChange={handleFieldsChange} name=" titulo" className="form-control" id="titulo" aria-describedby="emailHelp" placeholder="Título" required/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="descricao">Descrição</label>
-                    <input type="text" value={list.descricao} onChange={handleFieldsChange}  name='descricao' className="form-control" id="descricao" aria-describedby="emailHelp" placeholder="Descrição" required/>
-                </div>
-                <div className="form-group">
+            <div className="titulo-h1">
+                <h1>Editar Tarefa</h1>
+            </div>
+            <div className="cadastro">
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="titulo">Título</label>
+                        <input
+                            type="text"
+                            value={list.titulo}
+                            onChange={handleFieldsChange}
+                            name=" titulo"
+                            className="form-control"
+                            id="titulo"
+                            aria-describedby="emailHelp"
+                            placeholder="Título"
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="descricao">Descrição</label>
+                        <input
+                            type="text"
+                            value={list.descricao}
+                            onChange={handleFieldsChange}
+                            name="descricao"
+                            className="form-control"
+                            id="descricao"
+                            aria-describedby="emailHelp"
+                            placeholder="Descrição"
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
                         <label htmlFor="prioridade">Prioridade: </label>
-                        <select name="prioridade" value={list.prioridade} onChange={handleFieldsChange} className="form-select" >
-                            <option value="nenhum" >Nenhum</option>
+                        <select
+                            name="prioridade"
+                            value={list.prioridade}
+                            onChange={handleFieldsChange}
+                            className="form-select"
+                        >
+                            <option value="nenhum">Nenhum</option>
                             <option value="Alto">Alto</option>
                             <option value="Médio">Médio</option>
                             <option value="Baixa">Baixa</option>
                         </select>
-                </div>
-                <div className="form-group">
+                    </div>
+                    <div className="form-group">
                         <label htmlFor="status">Status: </label>
-                        <select name="status" value={list.status} onChange={handleFieldsChange} className="form-select" >
-                            <option value="nenhum" >Nenhum</option>
+                        <select
+                            name="status"
+                            value={list.status}
+                            onChange={handleFieldsChange}
+                            className="form-select"
+                        >
+                            <option value="nenhum">Nenhum</option>
                             <option value="Fazer">Fazer</option>
                             <option value="Fazendo">Fazendo</option>
                             <option value="Feito">Feito</option>
                         </select>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="prazo">Prazo</label>
-                    <input type="Date" name= "prazo" value={list.prazo} onChange={handleFieldsChange} className="form-control" id="prazo" aria-describedby="emailHelp" placeholder="Prazo"/>
-                </div>
-             
-                <br></br>
-                <button type="submit" title="Editar" className="btn bg-transparent">Editar</button>
-                <Link to={`/view/${id}`} type="button" title="Voltar" className="btn bg-transparent">
-                  < IoIosReturnLeft size={40} />
-                </Link>
-            </form>
-        </div>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="prazo">Prazo</label>
+                        <input
+                            type="Date"
+                            name="prazo"
+                            value={list.prazo}
+                            onChange={handleFieldsChange}
+                            className="form-control"
+                            id="prazo"
+                            aria-describedby="emailHelp"
+                            placeholder="Prazo"
+                        />
+                    </div>
+
+                    <br></br>
+                    <button
+                        type="submit"
+                        title="Editar"
+                        className="btn bg-transparent"
+                    >
+                        Editar
+                    </button>
+                    <Link
+                        to={`/view/${id}`}
+                        type="button"
+                        title="Voltar"
+                        className="btn bg-transparent"
+                    >
+                        <IoIosReturnLeft size={40} />
+                    </Link>
+                </form>
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default Edit
-
+export default Edit;
--- src/pages/View/index.js
+++ src/pages/View/index.js
@@ -1,44 +1,78 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Api from '../../api/api';
 import './View.css';
 import { Link } from 'react-router-dom';
-import { AiOutlineOrderedList } from "react-icons/ai";
-import { AiOutlineReconciliation } from "react-icons/ai";
-import { ImCalendar } from "react-icons/im";
-import { RiDeleteBinLine } from "react-icons/ri";
-import { FiEdit } from "react-icons/fi";
-import { IoIosReturnLeft} from "react-icons/io";
+import { AiOutlineOrderedList } from 'react-icons/ai';
+import { AiOutlineReconciliation } from 'react-icons/ai';
+import { ImCalendar } from 'react-icons/im';
+import { FiEdit } from 'react-icons/fi';
+import { IoIosReturnLeft } from 'react-icons/io';
+import ModalDelete from '../../components/structure/Modal';
 
 const View = () => {
     const [list, setList] = useState({});
-   
+
     useEffect(() => {
         getListById();
-      }, [])
+    }, []);
 
-    const { id }= useParams();
+    const { id } = useParams();
     console.log(id);
     const getListById = async () => {
         const request = await Api.fetchGetById(id);
         const list = await request.json();
         setList(list);
-        }
+    };
 
-         const meses = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul","Ago","Set","Out","Nov","Dez"];
-            let data = new Date(list.prazo);
-            let dataFormatada = ((data.getDate() + " " + meses[(data.getMonth())] + " " + data.getFullYear()));
+    const meses = [
+        'Jan',
+        'Fev',
+        'Mar',
+        'Abr',
+        'Mai',
+        'Jun',
+        'Jul',
+        'Ago',
+        'Set',
+        'Out',
+        'Nov',
+        'Dez',
+    ];
+    let data = new Date(list.prazo);
+    let dataFormatada =
+        data.getDate() +
+        ' ' +
+        meses[data.getMonth()] +
+        ' ' +
+        data.getFullYear();
 
-            const meses1 = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul","Ago","Set","Out","Nov","Dez"];
-            let data1 = new Date(list.dataCriacao);
-            let dataFormatada1 = ((data1.getDate() + " " + meses1[(data1.getMonth())] + " " + data1.getFullYear()));
+    const meses1 = [
+        'Jan',
+        'Fev',
+        'Mar',
+        'Abr',
+        'Mai',
+        'Jun',
+        'Jul',
+        'Ago',
+        'Set',
+        'Out',
+        'Nov',
+        'Dez',
+    ];
+    let data1 = new Date(list.dataCriacao);
+    let dataFormatada1 =
+        data1.getDate() +
+        ' ' +
+        meses1[data1.getMonth()] +
+        ' ' +
+        data1.getFullYear();
 
-
-        var date1 = new Date(data);
-        var date2 = new Date();
-        var timeDiff = Math.abs(date2.getTime() - date1.getTime());
-        var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-
+    var date1 = new Date(data);
+    var date2 = new Date();
+    var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
     return (
         <div>
@@ -46,26 +80,70 @@ const View = () => {
                 <h2>CHECK? LIST! </h2>
             </div>
             <div className="card-view ">
-                <h5 className="card-title">{list.titulo} 
-                <div className="edit-del">
-                    <Link to={`/edit/${id}`} type="button" class="btn  bg-transparent btn-sm">< FiEdit size={20}/></Link>
-                    <button type="button" class="btn bg-transparent btn-sm">< RiDeleteBinLine size={20} /></button>
-                    <Link to={'/'} title="Voltar" type="button" class="btn bg-transparent btn-sm"> < IoIosReturnLeft size={20}/> </Link>
-                </div></h5>
+                <h5 className="card-title">
+                    {list.titulo}
+                    <div className="edit-del">
+                        <Link
+                            to={`/edit/${id}`}
+                            type="button"
+                            className="btn  editar bg-transparent btn-sm"
+                        >
+                            <FiEdit size={20} />
+                        </Link>
+                        <button type="button" className="btn delete bg-transparent btn-sm">
+                            < ModalDelete />
+                        </button>
+                        <Link
+                            to={'/'}
+                            title="Voltar"
+                            type="button"
+                            className="btn voltar bg-transparent btn-sm"
+                        >
+                            {' '}
+                            <IoIosReturnLeft size={20} />{' '}
+                        </Link>
+                    </div>
+                </h5>
                 <p className="card-text-view">{list.descricao}</p>
                 <div className="icons-view">
-                <span title="Prioridade"><AiOutlineOrderedList size={35} /> <span className="badge bg-transparent text-dark">{list.prioridade}</span></span>
-                <span title="Status"><AiOutlineReconciliation size={35} /><span className="badge bg-transparent text-dark">{list.status}</span></span> 
-                <span title="Prazo"><ImCalendar size={28} /> <h2 className="badge bg-transparent text-dark">{dataFormatada}</h2> </span>
+                    <span title="Prioridade">
+                        <AiOutlineOrderedList size={35} />{' '}
+                        <span className="badge bg-transparent text-dark">
+                            {list.prioridade}
+                        </span>
+                    </span>
+                    <span title="Status">
+                        <AiOutlineReconciliation size={35} />
+                        <span className="badge bg-transparent text-dark">
+                            {list.status}
+                        </span>
+                    </span>
+                    <span title="Prazo">
+                        <ImCalendar size={28} />{' '}
+                        <h2 className="badge bg-transparent text-dark">
+                            {dataFormatada}
+                        </h2>{' '}
+                    </span>
                 </div>
-                <div className= "data-criacao">
-                <h5 title="Prazo"> Registrado:<h2 className="badge bg-transparent text-dark">{dataFormatada1}</h2></h5>
-                <h5 title="Prazo"> Restam:<h2 className="badge bg-transparent text-dark">{diffDays} dias</h2></h5>
+                <div className="data-criacao">
+                    <h5 title="Prazo">
+                        {' '}
+                        Registrado:
+                        <h2 className="badge bg-transparent text-dark">
+                            {dataFormatada1}
+                        </h2>
+                    </h5>
+                    <h5 title="Prazo">
+                        {' '}
+                        Restam:
+                        <h2 className="badge bg-transparent text-dark">
+                            {diffDays} dias
+                        </h2>
+                    </h5>
                 </div>
-
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default View
+export default View;
